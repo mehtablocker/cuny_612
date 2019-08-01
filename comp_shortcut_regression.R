83874f23f4102a89a3e12abccb4c60c7a592ed25
mehtablocker <46001827+mehtablocker@users.noreply.github.com>
2019-08-01 12:56:21 -0400

    Add files via upload

---
+++ comp_shortcut_regression.R
@@ -0,0 +1,19 @@
+# Computational shortcut to linear regression
+# (helpful when in very high dimensions and (A^T A) and (A^T b) are easy to manually create)
+
+### Random data
+n <- 100
+p <- 4
+A <- matrix(rnorm(n*p, 1, 0.1), nrow = n, ncol = p)
+b <- rnorm(n, 0, 4)
+
+### Solve
+cov_Ab <- (1/n) * t(A) %*% b - cbind(colMeans(A)) %*% cbind(mean(b))
+var_A <- (1/n) * t(A) %*% A - cbind(colMeans(A)) %*% colMeans(A)
+coefs <- solve(var_A) %*% cov_Ab
+slope <- mean(b) - colMeans(A) %*% coefs
+
+### Compare to lm
+slope
+coefs
+summary(lm(b ~ A))$coefficients[,1]
